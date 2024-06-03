aada91d6a6f54905314956ce5058b82e57809945
Bastien Guillaume <bguillau@student.42.fr>
2024-06-03 19:57:06 +0200

    debut config file

--- RequestParsing/main.cpp
+++ RequestParsing/main.cpp
@@ -5,10 +5,15 @@ int	main(void)
 	std::string	data[] = {
 		"POST http://localhost:80/home.html?a=1&b=2&c=3&d=4#fragment HTTP/1.1\r\nHost: localhost:8080\ncontent-length: 2lines\nformat: text\n\nthis is the body firstline\nthis is the body secondline (with a final lf)\n",
 		"DELETE /home.html HTTP/1.1\r\nHost: localhost:8080\ntruc: value\nmachin: chose\n\n",
-		"GET /home.html HTTP/1.1\r\nHost: localhost:8080\n\n"
+		"GET /home.html HTTP/1.1\r\nHost: localhost:8080\n\n",
+		"HEAD /home.html HTTP/1.1\r\nHost: localhost:8080\n\n",
+		"grosschiasse qui pue",
+		"GET / / / / // / / / // \r\n",
+		"PUT \n\n bad truc host:port\r\nbodybad\n",
+		"HEAD \r\n"
 	};
 
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < 8; ++i)
 	{	
 		Request	rq(data[i]);
 		rq.print();
