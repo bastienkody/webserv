549d0bc9c8d584c807838a849f1b369cb8eed3d8
Matthys MUESSER <mmuesser@paul-f4Ar6s6.clusters.42paris.fr>
2024-06-05 18:30:15 +0200

    add comment

--- server/CGI.cpp
+++ server/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: mmuesser <mmuesser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:31:43 by mmuesser          #+#    #+#             */
-/*   Updated: 2024/06/05 18:27:05 by mmuesser         ###   ########.fr       */
+/*   Updated: 2024/06/05 18:30:01 by mmuesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 recuperer Request object
 check si j'ai tout body via content_length (en checkant _header dans Request obj)
 creer pipe et dup fd pour que resultat script soit recuperable vu qu'il va ecrire sur stdin
-check verb pour exec script correspondant
+check verb (voir url selon les scripts qu'on aura) pour exec script correspondant
 fork puis launch execve
 */
 
