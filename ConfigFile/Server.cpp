6eaa28b3d608c8c47bcbb80c34777dc20f85b26d
Bastien Guillaume <bguillau@student.42.fr>
2024-06-10 18:31:24 +0200

    je vais bosser

--- ConfigFile/Server.cpp
+++ ConfigFile/Server.cpp
@@ -6,13 +6,10 @@ Server::~Server() {}
 //	os stream << redefinition
 std::ostream & operator<<(std::ostream& os, const Server &rhs)
 {
-	return (os << "Server " << rhs.getName() << " listens on " << rhs.getIp() << ":" << rhs.getPort());
+	return (os << "Server listens on " << rhs.getIp() << ":" << rhs.getPort());
 }
 //	Param constructor
-Server::Server(const std::string name)
-{
-	_name = name;
-}
+Server::Server(const std::string data) {}
 
 //	Copy
 Server::Server(const Server & src) {*this = src;}
@@ -20,23 +17,31 @@ Server & Server::operator=(const Server & rhs)
 {
 	if(this != &rhs)
 	{
-		_name = rhs.getName();
+		_names = rhs.getNames();
 		_ip = rhs.getIp();
 		_port = rhs.getPort();
 		_root = rhs.getRoot();
-		_autoindex = rhs.getAutoindex();
 		_index = rhs.getIndex();
+		_autoindex = rhs.getAutoindex();
+		_maxBodysize = rhs.getMaxBodySize();
+		_cgiPathes = rhs.getCgiExt();
+		_cgiExt = rhs.getCgiExt();
+		_allowMethods = rhs.getAllowMethods();
 	}
 	return (*this);
 }
 
 //	Getters
-std::string const & Server::getName() const	{return (_name);}
+std::vector<std::string> const & Server::getNames() const	{return (_names);}
 std::string const & Server::getIp() const	{return (_ip);}
 std::string const & Server::getPort() const	{return (_port);}
 std::string const & Server::getRoot() const	{return (_root);}
 std::string const & Server::getAutoindex() const	{return (_autoindex);}
 std::string const & Server::getIndex() const	{return (_index);}
+std::string const & Server::getMaxBodySize() const	{return (_maxBodysize);}
+std::string const & Server::getAllowMethods() const	{return (_allowMethods);}
+std::vector<std::string>const & Server::getCgiPathes() const	{return (_cgiPathes);}
+std::vector<std::string> const & Server::getCgiExt() const	{return (_cgiExt);}
 
 
 
--- ConfigFile/Server.hpp
+++ ConfigFile/Server.hpp
@@ -14,23 +14,28 @@ class Server
 		Server(const Server & src);
 		Server & operator=(const Server & rhs);
 
-		std::string const & getName() const;
+		std::vector<std::string> const & getNames() const;
 		std::string const & getIp() const;
 		std::string const & getPort() const;
 		std::string const & getRoot() const;
-		std::string const & getAutoindex() const;
 		std::string const & getIndex() const;
+		std::string const & getAutoindex() const;
+		std::string const & getMaxBodySize() const;
+		std::string const & getAllowMethods() const;
+		std::vector<std::string> const & getCgiPathes() const;
+		std::vector<std::string> const & getCgiExt() const;
 
 	private:
-		std::string _name;
-
+		std::vector<std::string> _names;
 		std::string _ip;
 		std::string _port;
-
 		std::string _root;
-
-		std::string	_autoindex;
 		std::string	_index;
+		std::string	_autoindex;
+		std::string	_maxBodysize;	//stored as a string but ovlf checks needed when reading + use of neg (not zero) if not provided
+		std::string	_allowMethods;
+		std::vector<std::string> _cgiPathes;
+		std::vector<std::string> _cgiExt;
 
 
 
