c427dacb82f6946be73a00b43443cedaee188e52
mmuesser <matthys.muesser@hotmail.com>
2024-09-23 18:19:51 +0200

    upload fix (j'espere)

--- server/CGI.cpp
+++ server/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: mmuesser <mmuesser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 17:50:31 by mmuesser          #+#    #+#             */
-/*   Updated: 2024/09/20 13:48:43 by mmuesser         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:12:29 by mmuesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,9 +78,11 @@ void	CGI::exec_son(int *pipe_fd, std::string path)
 	char **av = create_av();
 	(void) path;
 	/*check premiere ligne script ???*/
-	// execve(path.c_str(), av, env);
+	std::cerr<< "cgi path : " << path.c_str() << std::endl;
+	execve(path.c_str(), av, env);
 	free_tab(env);
 	free_tab(av);
+	std::cerr<< errno<<std::endl;
 	perror("Execve");
 	exit(-1);
 }
--- www/cgi-bin/formulaire.py
+++ www/cgi-bin/formulaire.py
@@ -1,1 +1,22 @@
-name=w3r&age=98
+#!/usr/bin/python3
+
+# Import modules for CGI handling
+import cgi, cgitb
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+name = form.getvalue('name')
+age = form.getvalue('age')
+
+
+print ("<html>")
+print ('<head>')
+print ("<title>Reponse Formulaire</title>")
+print ('</head>')
+print ('<body>')
+print (f"<h1>Bonjour, {name} !</h1>")
+print (f"<p>Vous avez {age} ans.</p>")
+print ('</body>')
+print ('</html>')
